ce718bfd0728fdb4b4615b41236979983d474878
dedipya20 <135147911+dedipya20@users.noreply.github.com>
2024-09-30 15:33:14 +0530

    producer.py

---
+++ producer.py
@@ -0,0 +1,26 @@
+# producer.py
+from kafka import KafkaProducer
+import json
+import time
+import pandas as pd
+
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+# Load data using pandas (replace 'your_file.csv' with the actual file)
+df = pd.read_csv('stock_data_new.csv')
+
+# Sending each row as a message to the Kafka topic
+for index, row in df.iterrows():
+    data = {
+        'index_name': row['Index'],
+        'timestamp': row['Timestamp'],
+        'stock_price': row['Price'],
+        'trading_volume': row['Volume']
+    }
+    producer.send('stock_topic', value=data)
+    time.sleep(1)
+
+producer.flush()
